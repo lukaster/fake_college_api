6b84bdc3d723d5bbc2d97ca50c29a707c2994998
lukaster <tereza.lukasova@centrum.cz>
2023-04-18 11:12:23 +0200

    testing with db=array

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Joi = require('joi');//load joi module - returns class
 const express = require('express') //returns function
 
 const app = express()//returns object type Express, name it app because convention
-const courses = require('./routes/courses_db')
+const courses = require('./routes/courses')
 
 
 app.use(express.static('public'))
@@ -32,7 +32,7 @@ app.get('/api/posts/:year/:month', (req, res) => { //parameter defined by : and 
 })
 
 //PORT
-console.log("process.env ", process.env)
+//console.log("process.env ", process.env)
 const port = process.env.PORT || 3000 // global object process, property env - environment variable, PORT - is used if it is set otherwise 3000
 app.listen(port, () => { console.log('listening on port ' + port) }) //fnc when apps starts listening on given port
 
